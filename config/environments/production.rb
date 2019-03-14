18466b6ba13b65de2899cd8b64041345b864b851
nullcline <hi.suguru@gmail.com>
2019-03-15 08:41:10 +0900

    Fix host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://thawing-mountain-89942.herokuapp.com'
+  host = 'https://rails-tutorial-advanced.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
